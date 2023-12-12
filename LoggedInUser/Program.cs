62b4c9e3ecf75de97651dc17ce29a3abe5735e3f
Krisztian Ivan <krisztian.ivan@gmail.com>
2023-12-12 12:06:37 +0000

    added test app

---
+++ LoggedInUser/Program.cs
@@ -0,0 +1,17 @@
+﻿try
+{
+    using (var searcher = new System.Management.ManagementObjectSearcher("Select * from Win32_ComputerSystem"))
+    {
+        using (var items = searcher.Get())
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
